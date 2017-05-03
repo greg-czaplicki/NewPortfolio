16e06294db499a01aa4cd287b114a052e26b08c3
glc3344 <glc3344@gmail.com>
2017-05-03 12:19:12 -0400

    feat: Add scroll spy and revealJs

--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
+    $('.scrollspy').scrollSpy();
+
+    window.sr = ScrollReveal();
+    sr.reveal('.img-portfolio', {duration: 2000, viewFactor: 0.5, scale: 0.8});
+    sr.reveal('.box', {duration: 2000, viewFactor: 0.5, scale: 0.8});
 });
